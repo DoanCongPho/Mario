270630ff31f6bd8132525b3de11d9b46fbae0059
Four-Beginner <congpho115@gmail.com>
2024-12-12 13:48:49 +0700

    loading map

---
+++ src/ResourcesManager.cpp
@@ -0,0 +1,8 @@
+//
+//  ResourcesManager.cpp
+//  Mario
+//
+//  Created by Đoàn Công Pho on 12/12/24.
+//
+
+#include "ResourcesManager.hpp"
---
+++ src/ResourcesManager.hpp
@@ -0,0 +1,13 @@
+//
+//  ResourcesManager.hpp
+//  Mario
+//
+//  Created by Đoàn Công Pho on 12/12/24.
+//
+
+#ifndef ResourcesManager_hpp
+#define ResourcesManager_hpp
+
+#include <stdio.h>
+
+#endif /* ResourcesManager_hpp */
