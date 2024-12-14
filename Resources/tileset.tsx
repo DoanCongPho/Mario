4192c9bbdf7933765c28f54ebd61ddfd019b3f7d
Four-Beginner <congpho115@gmail.com>
2024-12-14 20:05:22 +0700

    Updating detect collision (but can't jump  smothly) + adding object for collision for map 1

--- Resources/tileset.tsx
+++ Resources/tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="tileset1" tilewidth="32" tileheight="32" tilecount="78" columns="0">
+<tileset version="1.10" tiledversion="1.11.0" name="tileset" tilewidth="64" tileheight="50" tilecount="137" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image source="images/tiles/1/tile_A.png" width="32" height="32"/>
@@ -234,5 +234,182 @@
  </tile>
  <tile id="77">
   <image source="images/tiles/3/tile_Z.png" width="32" height="32"/>
+ </tile>
+ <tile id="78">
+  <image source="images/tiles/4/tile_A.png" width="32" height="32"/>
+ </tile>
+ <tile id="79">
+  <image source="images/tiles/4/tile_B.png" width="32" height="32"/>
+ </tile>
+ <tile id="80">
+  <image source="images/tiles/4/tile_C.png" width="32" height="32"/>
+ </tile>
+ <tile id="81">
+  <image source="images/tiles/4/tile_D.png" width="32" height="32"/>
+ </tile>
+ <tile id="82">
+  <image source="images/tiles/4/tile_E.png" width="32" height="32"/>
+ </tile>
+ <tile id="83">
+  <image source="images/tiles/4/tile_F.png" width="32" height="32"/>
+ </tile>
+ <tile id="84">
+  <image source="images/tiles/4/tile_G.png" width="32" height="32"/>
+ </tile>
+ <tile id="85">
+  <image source="images/tiles/4/tile_H.png" width="32" height="32"/>
+ </tile>
+ <tile id="86">
+  <image source="images/tiles/4/tile_I.png" width="32" height="32"/>
+ </tile>
+ <tile id="87">
+  <image source="images/tiles/4/tile_J.png" width="32" height="32"/>
+ </tile>
+ <tile id="88">
+  <image source="images/tiles/4/tile_K.png" width="32" height="32"/>
+ </tile>
+ <tile id="89">
+  <image source="images/tiles/4/tile_L.png" width="32" height="32"/>
+ </tile>
+ <tile id="90">
+  <image source="images/tiles/4/tile_M.png" width="32" height="32"/>
+ </tile>
+ <tile id="91">
+  <image source="images/tiles/4/tile_N.png" width="32" height="32"/>
+ </tile>
+ <tile id="92">
+  <image source="images/tiles/4/tile_O.png" width="32" height="32"/>
+ </tile>
+ <tile id="93">
+  <image source="images/tiles/4/tile_P.png" width="32" height="32"/>
+ </tile>
+ <tile id="94">
+  <image source="images/tiles/4/tile_Q.png" width="32" height="32"/>
+ </tile>
+ <tile id="95">
+  <image source="images/tiles/4/tile_R.png" width="32" height="32"/>
+ </tile>
+ <tile id="96">
+  <image source="images/tiles/4/tile_S.png" width="32" height="32"/>
+ </tile>
+ <tile id="97">
+  <image source="images/tiles/4/tile_T.png" width="32" height="32"/>
+ </tile>
+ <tile id="98">
+  <image source="images/tiles/4/tile_U.png" width="32" height="32"/>
+ </tile>
+ <tile id="99">
+  <image source="images/tiles/4/tile_V.png" width="32" height="32"/>
+ </tile>
+ <tile id="100">
+  <image source="images/tiles/4/tile_W.png" width="32" height="32"/>
+ </tile>
+ <tile id="101">
+  <image source="images/tiles/4/tile_X.png" width="32" height="32"/>
+ </tile>
+ <tile id="102">
+  <image source="images/tiles/4/tile_Y.png" width="32" height="32"/>
+ </tile>
+ <tile id="103">
+  <image source="images/tiles/4/tile_Z.png" width="32" height="32"/>
+ </tile>
+ <tile id="104">
+  <image source="images/sprites/blocks/Cloud_0.png" width="32" height="32"/>
+ </tile>
+ <tile id="105">
+  <image source="images/sprites/blocks/Exclamation_0.png" width="32" height="32"/>
+ </tile>
+ <tile id="106">
+  <image source="images/sprites/blocks/EyesClosed_0.png" width="32" height="32"/>
+ </tile>
+ <tile id="107">
+  <image source="images/sprites/blocks/EyesOpened_0.png" width="32" height="32"/>
+ </tile>
+ <tile id="108">
+  <image source="images/sprites/blocks/EyesOpened_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="109">
+  <image source="images/sprites/blocks/EyesOpened_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="110">
+  <image source="images/sprites/blocks/EyesOpened_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="111">
+  <image source="images/sprites/blocks/Glass_0.png" width="32" height="32"/>
+ </tile>
+ <tile id="112">
+  <image source="images/sprites/blocks/Message_0.png" width="32" height="32"/>
+ </tile>
+ <tile id="113">
+  <image source="images/sprites/blocks/Question_0.png" width="32" height="32"/>
+ </tile>
+ <tile id="114">
+  <image source="images/sprites/blocks/Question_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="115">
+  <image source="images/sprites/blocks/Question_2.png" width="32" height="32"/>
+ </tile>
+ <tile id="116">
+  <image source="images/sprites/blocks/Question_3.png" width="32" height="32"/>
+ </tile>
+ <tile id="117">
+  <image source="images/sprites/blocks/Stone_0.png" width="32" height="32"/>
+ </tile>
+ <tile id="118">
+  <image source="images/sprites/blocks/Wood_0.png" width="32" height="32"/>
+ </tile>
+ <tile id="119">
+  <image source="images/sprites/items/BeanstalkHead_0.png" width="32" height="32"/>
+ </tile>
+ <tile id="120">
+  <image source="images/sprites/items/BeanstalkHead_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="121">
+  <image source="images/sprites/items/BeanstalkHeadLoop.png" width="32" height="32"/>
+ </tile>
+ <tile id="122">
+  <image source="images/sprites/items/Coin_0.png" width="24" height="32"/>
+ </tile>
+ <tile id="123">
+  <image source="images/sprites/items/Coin_1.png" width="24" height="32"/>
+ </tile>
+ <tile id="124">
+  <image source="images/sprites/items/Coin_2.png" width="24" height="32"/>
+ </tile>
+ <tile id="125">
+  <image source="images/sprites/items/Coin_3.png" width="24" height="32"/>
+ </tile>
+ <tile id="126">
+  <image source="images/sprites/items/CourseClearToken.png" width="64" height="32"/>
+ </tile>
+ <tile id="127">
+  <image source="images/sprites/items/FireFlower_0.png" width="32" height="32"/>
+ </tile>
+ <tile id="128">
+  <image source="images/sprites/items/FireFlower_1.png" width="32" height="32"/>
+ </tile>
+ <tile id="129">
+  <image source="images/sprites/items/Mushroom.png" width="32" height="32"/>
+ </tile>
+ <tile id="130">
+  <image source="images/sprites/items/Star.png" width="30" height="32"/>
+ </tile>
+ <tile id="131">
+  <image source="images/sprites/items/YoshiCoin_0.png" width="32" height="50"/>
+ </tile>
+ <tile id="132">
+  <image source="images/sprites/items/YoshiCoin_1.png" width="32" height="50"/>
+ </tile>
+ <tile id="133">
+  <image source="images/sprites/items/YoshiCoin_2.png" width="32" height="50"/>
+ </tile>
+ <tile id="134">
+  <image source="images/sprites/items/YoshiCoin_3.png" width="32" height="50"/>
+ </tile>
+ <tile id="135">
+  <image source="images/sprites/items/1UpMushroom.png" width="32" height="32"/>
+ </tile>
+ <tile id="136">
+  <image source="images/sprites/items/3UpMoon.png" width="30" height="32"/>
  </tile>
 </tileset>
